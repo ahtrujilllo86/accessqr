4b4a3e692890253c8f31ec6859e538476d68f305
unknown <ahtrujillo86@gmail.com>
2022-09-04 12:04:59 -0500

    templatehead added

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <title>QR Access</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-       
-        <link rel="stylesheet" type="text/css" href="../public/css/estilos.css">
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="../public/js/funciones.js"></script>
-        <script type="text/javascript" src="../public/js/qrcode.js"></script>
-    </head>
+@extends('templatehead')
+@section('content')
 <body>
     <div class="container-fluid maindiv">
         <div class="row">
@@ -54,3 +39,4 @@
     </div>
 </body>
 </html>
+@endsection
