247f6c59d0fe66ab0ee3c6dd920cb6b456f3717e
unknown <ahtrujillo86@gmail.com>
2022-09-05 10:27:37 -0500

    mail fail test

--- app/Http/Controllers/RecoverController.php
+++ app/Http/Controllers/RecoverController.php
@@ -18,7 +18,7 @@ class RecoverController extends Controller
 
         $para = $request->recovermail;
         $gettoken = Usuarios::where('correo', $para)->get('remember_token')->first();
-        //return $gettoken;
+        return $gettoken;
         if(!$gettoken){
             $errormsg = "El correo no existe";
             //return view('recoverpwd')->with('error', "el correo no existe en la base");
